78693f27d286178aef9a2f87cfc5ae71e901fd64
Lv Jingxin <lv510987@163.com>
2023-08-23 11:47:18 +0800

    feat: printf练习

--- 03.bltcalc/main.go
+++ 03.bltcalc/main.go
@@ -1,22 +1,30 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	a, b := 100, 31
-	fmt.Println(a ^ b)
-	fmt.Println(b ^ a)
+	//a, b := 100, 31
+	//fmt.Println(a ^ b)
+	//fmt.Println(b ^ a)
+	//
+	//arr := []int{4, 5, 4, 5, 6, 7, 6, 3, 3}
+	//result := -1
+	//for _, item := range arr {
+	//	if result < 0 {
+	//		result = item
+	//	} else {
+	//		result = result ^ item
+	//	}
+	//}
+	//fmt.Println(result)
 
-	arr := []int{4, 5, 4, 5, 6, 7, 6, 3, 3}
-	result := -1
-	for _, item := range arr {
-		if result < 0 {
-			result = item
-		} else {
-			result = result ^ item
-		}
-	}
-	fmt.Println(result)
+	a := 2
+	b := 10101010
+
+	//a = a + b
+	//b = a - b
+	//a = a - b
+	fmt.Printf("%b", a)
+	fmt.Println("b = ", *&b)
+
 }
---
+++ 04.strdmt/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("中文 abc ❤️")
+	fmt.Printf("我的名字是%s\n", "小强")
+	fmt.Printf("我的名字是%q\n", "小强")
+	fmt.Printf("我的名字是%x\n", "小强")
+	fmt.Printf("我的名字是%X\n", "小强")
+}
