b96f0e84c8dc7e79a07a14657651b549ef7a7525
Lv Jingxin <lv510987@163.com>
2023-08-25 15:26:36 +0800

    feat: 数组分片 make方法，长度len(),容量cap()

--- 11.practice/main.go
+++ 11.practice/main.go
@@ -14,8 +14,18 @@ func sortArray(arr []int) []int {
 	return arr
 }
 
+// 打印数组长度和容量
+func printSlice(x []int) {
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
+}
 func main() {
 	var arr = []int{1, 4, 3, 2, 5, 8, 7, 6}
 	fmt.Println(arr)
 	fmt.Println(sortArray(arr))
+	fmt.Println("==========分割线============")
+	printSlice(arr)
+	// make([]T, len, cap)   make函数创建分片 capacity容量作为可选参数 这里 length 是数组的长度并且也是切片的初始长度
+	slice1 := make([]int, 5, 1000)
+	slice1 = append(slice1, 1)
+	fmt.Println(slice1)
 }
