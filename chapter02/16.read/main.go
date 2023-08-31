a56b6c8605ac2e289a3d0b1d0af410cf030222c0
Lv Jingxin <lv510987@163.com>
2023-08-31 15:39:45 +0800

    feat: Go Module

--- chapter02/16.read/main.go
+++ chapter02/16.read/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	_ "github.com/spf13/cobra" // 类型转换
+	"github.com/spf13/cobra" // 类型转换的包
+	"learn.go/11.practice/01.fatrate.refactor/calc"
 )
 
 // 命令行定义
@@ -13,23 +12,33 @@ func main() {
 	var (
 		name   string
 		sex    string
-		tall   string // float64
-		weight string //float64
-		age    string //int
+		tall   float64
+		weight float64
+		age    int
 	)
 	//	os.Args 读取参数
-	arguments := os.Args
-	name = arguments[1]
-	sex = arguments[2]
-	tall = arguments[3]
-	weight = arguments[4]
-	age = arguments[5]
-	fmt.Println(arguments)
-	fmt.Println("name  ", name)
-	fmt.Println("sex   ", sex)
-	fmt.Println("tall  ", tall)
-	fmt.Println("weight", weight)
-	fmt.Println("age   ", age)
-	//	计算
-	//	评估结果
+	cmd := &cobra.Command{
+		Use:   "healthcheck",
+		Short: "体脂计算器，根据吧啦吧啦计算体脂，给出建议（此处写简短描述）",
+		Long:  "这个写详细描述",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("name  ", name)
+			fmt.Println("sex   ", sex)
+			fmt.Println("tall  ", tall)
+			fmt.Println("weight", weight)
+			fmt.Println("age   ", age)
+			//	计算
+			bmi := calc.CalcBMI(tall, weight)
+			fatRate := calc.CalcFatRate(bmi, age, sex)
+			fmt.Println("fatRate =", fatRate)
+			//	评估结果
+		},
+	}
+	cmd.Flags().StringVar(&name, "name", "", "姓名")
+	cmd.Flags().StringVar(&sex, "sex", "", "性别")
+	cmd.Flags().Float64Var(&tall, "tall", 0, "身高")
+	cmd.Flags().Float64Var(&weight, "weight", 0, "体重")
+	cmd.Flags().IntVar(&age, "age", 0, "年龄")
+
+	cmd.Execute()
 }
