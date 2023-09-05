1b944e1450f6e06d64aec3d95d00cab298672634
Lv Jingxin <lv510987@163.com>
2023-09-05 11:57:40 +0800

    feat: 新增案例三

--- 11.practice/03.tdd/main_test.go
+++ 11.practice/03.tdd/main_test.go
@@ -98,3 +98,46 @@ func TestCase2(t *testing.T) {
 		}
 	}
 }
+func TestCase3(t *testing.T) {
+	inputRecord("王强", 0.38)
+	inputRecord("李静", 0.28)
+	inputRecord("张伟")
+
+	{
+		randOfLJ, fatRateOfLJ := getRand("李静")
+		if randOfLJ != 1 {
+			//	把案例标记成失败但是还是继续会往下跑
+			//t.Fail()
+			//	结束之后直接退出
+			t.Fatalf("预期李静第一，但是得到的是：%d", randOfLJ)
+			return
+		}
+		if fatRateOfLJ != 0.28 {
+			t.Fatalf("预期李静的体脂是0.28，但的得到的是 %f", fatRateOfLJ)
+		}
+	}
+	{
+		randOfWQ, fatRateOfWQ := getRand("王强")
+		if randOfWQ != 2 {
+			//	把案例标记成失败但是还是继续会往下跑
+			//t.Fail()
+			//	结束之后直接退出
+			t.Fatalf("预期王强第二，但是得到的是：%d", randOfWQ)
+			return
+		}
+		if fatRateOfWQ != 0.38 {
+			t.Fatalf("预期王强的体脂是0.38，但的得到的是 %f", fatRateOfWQ)
+		}
+	}
+	{
+		randOfZW, _ := getRand("张伟")
+		if randOfZW != 3 {
+			//	把案例标记成失败但是还是继续会往下跑
+			//t.Fail()
+			//	结束之后直接退出
+			t.Fatalf("预期张伟第三，但是得到的是：%d", randOfZW)
+			return
+		}
+
+	}
+}
