8b8830b17844d7b82241fcc32050a71556ef8199
Lv Jingxin <lv510987@163.com>
2023-09-05 11:40:21 +0800

    feat: 完成案例一的需求测试

--- 11.practice/03.tdd/main_test.go
+++ 11.practice/03.tdd/main_test.go
@@ -2,6 +2,24 @@ package main
 
 import "testing"
 
+func TestCase1Part1(t *testing.T) {
+	inputRecord("王强", 0.38)
+	inputRecord("王强", 0.32)
+	{
+		randOfWQ, fatRateOfWQ := getRand("王强")
+		if randOfWQ != 1 {
+			//	把案例标记成失败但是还是继续会往下跑
+			//t.Fail()
+			//	结束之后直接退出
+			t.Fatalf("预期王强第一，但是得到的是：%d", randOfWQ)
+			return
+		}
+		if fatRateOfWQ != 0.32 {
+			t.Fatalf("预期王强的体脂是0.32，但的得到的是 %f", fatRateOfWQ)
+		}
+	}
+}
+
 func TestCase1(t *testing.T) {
 	inputRecord("王强", 0.38)
 	inputRecord("王强", 0.32)
